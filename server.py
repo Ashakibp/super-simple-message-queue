9b6a75247a36873559f6c0ee6ae86087c834f8ad
Ashakibp <aaronshakib@gmail.com>
2018-02-13 18:52:17 -0500

    Push

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from messageService import messageService
 import json
-from flask import request, Flask
+from flask import request, Flask, jsonify
 
 msgService = messageService()
 app = Flask(__name__)
@@ -8,7 +8,21 @@ app = Flask(__name__)
 @app.route("/newUser/<firstName>/<lastName>/<email>")
 def newUser(firstName, lastName, email):
   msgService.newUser(firstName, lastName, email)
-  return True
+  return "Nice"
+
+@app.route("/sendMessage/<email>/<sender>/<reciever>/<text>")
+def sendMessage(email, sender, reciever, text):
+    msgService.sendMessage(email, sender, reciever, text)
+    return "SENT"
+
+@app.route("/getMessages/<email>")
+def getMessages(email):
+    return jsonify({
+        email: msgService.getUser(email).messages
+    })
+
+
+
 
 
 app.run(host='0.0.0.0')
