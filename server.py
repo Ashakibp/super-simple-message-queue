f826a0ab1665d4ad0be4fe48b477a872c0627a7b
Ashakibp <aaronshakib@gmail.com>
2018-02-13 18:43:59 -0500

    Push

--- server.py
+++ server.py
@@ -5,10 +5,10 @@ from flask import request, Flask
 msgService = messageService()
 app = Flask(__name__)
 
-
 @app.route("/newUser/<firstName>/<lastName>/<email>")
 def newUser(firstName, lastName, email):
   msgService.newUser(firstName, lastName, email)
   return True
 
-  
+
+app.run(host='0.0.0.0')
