6293fa9949772d57e419f875ca9f906bdb85437b
Aaron Shakibpanah <aaronshakib@gmail.com>
2018-02-13 18:37:06 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,14 @@
 from messageService import messageService
 import json
+from flask import request, Flask
 
 msgService = messageService()
+app = Flask(__name__)
+
+
+@app.route("/newUser/<firstName>/<lastName>/<email>")
+def newUser(firstName, lastName, email):
+  msgService.newUser(firstName, lastName, email)
+  return True
+
+  
